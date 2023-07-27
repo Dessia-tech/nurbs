7d31555773b15acb617f238fd5c52a5052aa2888
Steven Masfaraud <masfaraud@dessia.tech>
2023-07-27 16:51:04 +0200

    fix: setup from git tags

--- setup.py
+++ setup.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python
 
-# NURBS-Python - Copyright (c) 2016-2018 Onur Rauf Bingol
-# geomdl - Copyright (c) 2018-2019 Onur Rauf Bingol
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import os
+from os.path import dirname, isdir, join
 import re
 import shutil
 import sys
 from distutils.command.clean import clean as clean_command
+from subprocess import CalledProcessError, check_output
 
 from setuptools import Extension, setup
 from setuptools.command.install import install as install_command
@@ -177,6 +158,110 @@ def in_argv(arg_list):
     return False
 
 
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+tag_re = re.compile(r"\btag: %s([0-9][^,]*)\b")
+version_re = re.compile("^Version: (.+)$", re.M)
+
+
+def version_from_git_describe(version):
+    if version[0] == "v":
+        version = version[1:]
+    # PEP 440 compatibility
+    number_commits_ahead = 0
+    if "-" in version:
+        version, number_commits_ahead, commit_hash = version.split("-")
+        number_commits_ahead = int(number_commits_ahead)
+
+    split_versions = version.split(".")
+    if "post" in split_versions[-1]:
+        suffix = split_versions[-1]
+        split_versions = split_versions[:-1]
+    else:
+        suffix = None
+    for pre_release_segment in ["a", "b", "rc"]:
+        if pre_release_segment in split_versions[-1]:
+            if number_commits_ahead > 0:
+                split_versions[-1] = str(
+                    split_versions[-1].split(pre_release_segment)[0]
+                )
+                if len(split_versions) == 2:
+                    split_versions.append("0")
+                if len(split_versions) == 1:
+                    split_versions.extend(["0", "0"])
+                split_versions[-1] = str(int(split_versions[-1]) + 1)
+                future_version = ".".join(split_versions)
+                return "{}.dev{}+{}".format(future_version, number_commits_ahead, commit_hash)
+            else:
+                return ".".join(split_versions)
+    if number_commits_ahead > 0:
+        if len(split_versions) == 2:
+            split_versions.append("0")
+        if len(split_versions) == 1:
+            split_versions.extend(["0", "0"])
+        split_versions[-1] = str(int(split_versions[-1]) + 1)
+        split_versions = ".".join(split_versions)
+        return "{}.dev{}+{}".format(split_versions, number_commits_ahead, commit_hash)
+    else:
+        if suffix is not None:
+            split_versions.append(suffix)
+        return ".".join(split_versions)
+
+
+# Just testing if get_version works well
+assert version_from_git_describe("v0.1.7.post2") == "0.1.7.post2"
+assert version_from_git_describe("v0.0.1-25-gaf0bf53") == "0.0.2.dev25+gaf0bf53"
+assert version_from_git_describe("v0.1-15-zsdgaz") == "0.1.1.dev15+zsdgaz"
+assert version_from_git_describe("v1") == "1"
+assert version_from_git_describe("v1-3-aqsfjbo") == "1.0.1.dev3+aqsfjbo"
+assert version_from_git_describe("v0.13rc0") == "0.13rc0"
+
+
+def get_version():
+    # Return the version if it has been injected into the file by git-archive
+    version = tag_re.search("$Format:%D$")
+    if version:
+        return version.group(1)
+    d = dirname(__file__)
+
+    if isdir(join(d, ".git")):
+        cmd = "git describe --tags"
+        try:
+            version = check_output(cmd.split()).decode().strip()[:]
+        except CalledProcessError:
+            raise RuntimeError("Unable to get version number from git tags")
+        version = version_from_git_describe(version)
+    else:
+        # Extract the version from the PKG-INFO file.
+        with open(join(d, "PKG-INFO")) as f:
+            version = version_re.search(f.read()).group(1)
+
+    # branch = get_branch()
+    # if branch and branch != 'master':
+    #     branch = re.sub('[^A-Za-z0-9]+', '', branch)
+    #     if '+' in version:
+    #         version += f'{branch}'
+    #     else:
+    #         version += f'+{branch}'
+
+    return version
+
+
+def get_branch():
+
+    if isdir(join(dirname(__file__), ".git")):
+        cmd = "git branch --show-current"
+        try:
+            return check_output(cmd.split()).decode().strip()[:]
+        except CalledProcessError:
+            pass
+
+    return None
+
+
 # Define setup.py commands to activate Cython compilation
 possible_cmds = ["install", "build", "bdist"]
 
@@ -242,7 +327,7 @@ if BUILD_FROM_CYTHON or BUILD_FROM_SOURCE:
 # Input for setuptools.setup
 data = dict(
     name=package_name,
-    version=get_property("__version__", package_dir),
+    version=get_version(),
     description=get_property("__description__", package_dir),
     long_description=read("DESCRIPTION.rst"),
     license=get_property("__license__", package_dir),
